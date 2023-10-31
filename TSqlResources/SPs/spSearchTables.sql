c31ee6ea452ecbb5e7b1a8dad90b0ed9748ecf20
Federico Di Marco <fededm76@hotmail.it>
2023-10-31 22:49:43 +0100

    significant improvements and bugfixes

--- TSqlResources/SPs/spSearchTables.sql
+++ TSqlResources/SPs/spSearchTables.sql
@@ -4,8 +4,10 @@ spSearchTables - A helper stored procedure which allows to search tables or colu
 
 PARAMETERS:
 	- @dbSearchPattern: a SQL LIKE pattern to filter databases, set to NULL to search in all databases
+	- @schemaSearchPattern: a SQL LIKE pattern to filter schemas, set to NULL to search in all schemas
 	- @tableSearchPattern: a SQL LIKE pattern to filter tables, set to  NULL to search in all tables
-	- @columnSearchPattern: a SQL LIKE pattern to filter columns, set to NULL to peform search only on tables, set to '%' to search also on all columns
+	- @columnTypeSearchPattern: a SQL LIKE pattern to filter column type, set to NULL to search in all column types
+	- @columnSearchPattern: a SQL LIKE pattern to filter columns, set to NULL to search in all columns
 	- @valuePattern: a SQL LIKE pattern to filter column value, set to NULL to not to search on column values
  
 OUTPUT:
@@ -18,23 +20,31 @@ A table with these columns:
   - [MatchingSelect]: the select statement returning the columns and rows matching the @valuePattern (it supports all column datatypes)
 
 SAMPLE SEARCHES:
-exec spSearchTables NULL,NULL,NULL,NULL - returns all tables with all columns in all databases in the server
-exec spSearchTables 'North%',NULL,NULL,NULL - returns all tables with all columns in all databases starting with North% in the server
-exec spSearchTables 'North%','S%',NULL,NULL - returns tables starting with S% with all columns in databases starting with North% in the server
-exec spSearchTables 'North%','S%','P%',NULL - returns tables starting with S% with columns starting with P% in databases starting with North% in the server 
-exec spSearchTables 'North%','S%','P%','30%' - returns tables starting with S% with columns starting with P% whose value matches 30% in databases starting with North% in the server
-exec spSearchTables NULL,NULL,NULL,'30%' - returns all table and all columns whose value matches 30% in all databases in the server
+exec spSearchTables NULL,NULL,NULL,NULL,NULL,NULL - returns all tables with all columns in all databases in the server
+exec spSearchTables 'North%','d%',NULL,NULL,NULL,NULL - returns all tables with all columns in all databases starting with North% and having d% in the schema in the server
+exec spSearchTables 'North%',NULL,'S%',NULL,NULL,NULL - returns tables starting with S% with all columns in databases starting with North% in the server
+exec spSearchTables 'North%',NULL,'S%','%int%',NULL,NULL - returns tables starting with S% with all columns in databases starting with North% in the server and type %int%
+exec spSearchTables 'North%',NULL,'S%',NULL,'P%',NULL - returns tables starting with S% with columns starting with P% in databases starting with North% in the server 
+exec spSearchTables 'North%',,NULL,'S%',NULL,'P%','30%' - returns tables starting with S% with columns starting with P% whose value matches 30% in databases starting with North% in the server
+exec spSearchTables NULL,NULL,NULL,NULL,NULL,'30%' - returns all table and all columns whose value matches 30% in all databases in the server
 
 VERSION HISTORY:
   20231021	fededim		Initial Release
   20231025	fededim		improved check for values now it uses just a single query for each table / flattened matching columns in a comma separated list / added MatchingColumns as first ones in the MatchingSelect query
   20231025	fededim		improved handling of @columnSearchPattern parameter, now you can use it without specifying @valuePattern parameter
+  20231031	fededim		BREAKING CHANGE:
+							- added schemaSearchPattern and columnTypeSearchPattern parameters
+							- bugfix and improvement on geometry and geography column types
+							- improvement on MatchingSelect output column: now it returns the converted columns used to perform where conditions
+							- WKT can be used in @valuePattern to perform STContains query on spatial columns where supported (e.g. database compatibility level >=130)
  
 *********************************************************************************************/
 
 CREATE OR ALTER PROCEDURE spSearchTables
 (@dbSearchPattern nvarchar(256)=NULL, 
+ @schemaSearchPattern nvarchar(256),
  @tableSearchPattern nvarchar(256),
+ @columnTypeSearchPattern nvarchar(256)=NULL,
  @columnSearchPattern nvarchar(256)=NULL,
  @valuePattern nvarchar(1000)=NULL)
 AS
@@ -50,6 +60,7 @@ CREATE TABLE #Output
  [Table] nvarchar(100),
  [FullTableName] nvarchar(300),
  [MatchingColumns] nvarchar(max),
+ [MatchingWhereColumns] nvarchar(max),
  [MatchingSelect] nvarchar(max),
 )
 
@@ -61,16 +72,29 @@ USE [?]
 
 DECLARE @dbName nvarchar(200), @schemaName nvarchar(200),@tableName nvarchar(200), @columnName nvarchar(200), @columnType nvarchar(200), @fullTableName nvarchar(1000)  -- current data
 DECLARE @oldDbName nvarchar(200), @oldSchemaName nvarchar(200),@oldTableName nvarchar(200), @oldFullTableName nvarchar(1000)  -- old data
-DECLARE @whereColumnName nvarchar(200), @whereCondition nvarchar(400), @whereClause nvarchar(max), @sql nvarchar(max), @selectSql nvarchar(max), @columnListSelect nvarchar(max)  -- helper variables
+DECLARE @whereColumnName nvarchar(200), @whereCondition nvarchar(400), @whereClause nvarchar(max), @sql nvarchar(max), @selectSql nvarchar(max), @columnListSelect nvarchar(max), @columnList nvarchar(max)  -- helper variables
+
+-- try to parse innerValuePattern with geometry and geography types
+DECLARE @geometry geometry, @geography geography, @compatibilityLevel int
+
+BEGIN TRY
+	SELECT @compatibilityLevel = compatibility_level FROM sys.databases WHERE database_id=DB_ID()
+	SELECT @geography=geography::Parse(@innerValuePattern)
+	SELECT @geometry=geometry::Parse(@innerValuePattern)
+END TRY
+BEGIN CATCH
+END CATCH
+
+
 
 PRINT N''Checking database [?]''
 
 DECLARE [tables] CURSOR LOCAL READ_ONLY FORWARD_ONLY FOR
 SELECT N''[''+ DB_NAME() +N'']'' AS DatabaseName,N''[''+ s.[name] +N'']'' AS SchemaName,N''[''+ t.[name] +N'']'' AS TableName,N''[''+ c.[name] +N'']'' AS ColumnName,tp.[Name] AS ColumnType, N''[''+DB_NAME()+N''].[''+s.[name]+N''].[''+t.[name]+N'']'' AS FullTableName
 FROM sys.tables t
-INNER JOIN sys.schemas s ON t.schema_id=s.schema_id
+INNER JOIN sys.schemas s ON t.schema_id=s.schema_id AND (@innerSchemaSearchPattern IS NULL OR s.[name] LIKE @innerSchemaSearchPattern)
 INNER JOIN sys.columns c ON (@innerColumnSearchPattern IS NULL OR c.[name] LIKE @innerColumnSearchPattern) AND c.[object_id]=t.[object_id]
-INNER JOIN sys.types tp ON tp.user_type_id = c.user_type_id'
+INNER JOIN sys.types tp ON tp.user_type_id = c.user_type_id AND (@innerColumnTypeSearchPattern IS NULL OR tp.[name] LIKE @innerColumnTypeSearchPattern)'
 +IIF(@tableSearchPattern IS NOT NULL, CHAR(10)+'WHERE t.[name] LIKE '''+@tableSearchPattern+'''','')
 +CHAR(10)+N'ORDER BY FullTableName
 
@@ -85,7 +109,7 @@ BEGIN
 	BEGIN
 		IF (@whereClause IS NOT NULL)
 		BEGIN
-			SET @whereClause = REPLACE(CONCAT(@whereClause,N''[???]'') COLLATE Latin1_General_CI_AS,N''OR [???]'',N'''')  --trim last "OR "
+			SET @whereClause = REPLACE(CONCAT(@whereClause,N''[???]'') COLLATE DATABASE_DEFAULT,N''OR [???]'',N'''')  --trim last "OR "
 			SET @selectSql = N''SELECT [??] FROM ''+@oldFullTableName+N'' WHERE '' + @whereClause 
 		END
 		ELSE
@@ -93,14 +117,15 @@ BEGIN
 
 		IF (@columnListSelect IS NOT NULL)
 		BEGIN
-			SET @columnListSelect = N''REPLACE(CONCAT(''+@columnListSelect + N''''''[???]'''') COLLATE Latin1_General_CI_AS,N'''',[???]'''',N'''''''')''  -- trim last ","
-			SET @sql = N'' INSERT INTO #Output SELECT  ''''''+@oldDbName+N'''''',''''''+@oldSchemaName+N'''''',''''''+@oldTableName+N'''''',''''''+@oldFullTableName+N'''''',''+@columnListSelect+N'',''''''+REPLACE(@selectSql COLLATE Latin1_General_CI_AS,'''''''','''''''''''')+N''''''''+IIF(@whereClause IS NOT NULL,'' FROM ''+@oldFullTableName+'' WHERE ''+@whereClause,'''')
+			SET @columnListSelect = N''REPLACE(CONCAT(''+@columnListSelect + N''''''[???]'''') COLLATE DATABASE_DEFAULT,N'''',[???]'''',N'''''''')''  -- trim last ","
+			SET @columnList = N''REPLACE(CONCAT(''+@columnList + N''''''[???]'''') COLLATE DATABASE_DEFAULT,N'''',[???]'''',N'''''''')''  -- trim last ","
+			SET @sql = N'' INSERT INTO #Output SELECT  ''''''+@oldDbName+N'''''',''''''+@oldSchemaName+N'''''',''''''+@oldTableName+N'''''',''''''+@oldFullTableName+N'''''',''+@columnList+N'',''+@columnListSelect+N'',''''''+REPLACE(@selectSql COLLATE DATABASE_DEFAULT,'''''''','''''''''''')+N''''''''+IIF(@whereClause IS NOT NULL,'' FROM ''+@oldFullTableName+'' WHERE ''+@whereClause,'''')
 		END
 		ELSE IF (@oldDbName IS NOT NULL)
-			SET @sql = N'' INSERT INTO #Output SELECT  ''''''+@oldDbName+N'''''',''''''+@oldSchemaName+N'''''',''''''+@oldTableName+N'''''',''''''+@oldFullTableName+N'''''',NULL,NULL''
+			SET @sql = N'' INSERT INTO #Output SELECT  ''''''+@oldDbName+N'''''',''''''+@oldSchemaName+N'''''',''''''+@oldTableName+N'''''',''''''+@oldFullTableName+N'''''',NULL,NULL,NULL''
 
 		IF (@selectSql IS NOT NULL)
-			SET @sql = N''IF EXISTS (''+REPLACE(@selectSql COLLATE Latin1_General_CI_AS,''[??]'',''1'')+N'')'' +@sql
+			SET @sql = N''IF EXISTS (''+REPLACE(@selectSql COLLATE DATABASE_DEFAULT,''[??]'',''1'')+N'')'' +@sql
 
 		IF (@sql IS NOT NULL)
 		BEGIN
@@ -114,21 +139,38 @@ BEGIN
 
 		SET @whereClause = NULL
 		SET @columnListSelect = NULL
+		SET @columnList = NULL
 	END
 
 	IF (@innerValuePattern IS NOT NULL)
 	BEGIN
-		SET @whereColumnName = (CASE WHEN @columnType COLLATE Latin1_General_CI_AS = N''image'' THEN N''CONVERT(NVARCHAR(MAX),CONVERT(VARBINARY(MAX),''+@columnName+N''),1)'' WHEN @columnType COLLATE Latin1_General_CI_AS = N''xml'' THEN N''CONVERT(nvarchar(MAX),''+@columnName+N'')'' WHEN @columnType COLLATE Latin1_General_CI_AS IN (N''hierarchyid'',N''geography'') THEN @columnName+N''.ToString()'' WHEN @columnType COLLATE Latin1_General_CI_AS IN (N''datetime'',N''datetime2'',N''datetimeoffset'',N''time'') THEN N''CONVERT(nvarchar(50),''+@columnName+N'',126)'' ELSE @columnName END)
-		SET @whereCondition = N''('' + @whereColumnName+N'' LIKE ''''''+@innerValuePattern+N'''''')''
+		SET @whereColumnName = (CASE WHEN @columnType COLLATE DATABASE_DEFAULT = N''image'' THEN N''CONVERT(NVARCHAR(MAX),CONVERT(VARBINARY(MAX),''+@columnName+N''),1)'' WHEN @columnType COLLATE DATABASE_DEFAULT = N''xml'' THEN N''CONVERT(nvarchar(MAX),''+@columnName+N'')'' WHEN @columnType COLLATE DATABASE_DEFAULT IN (N''hierarchyid'') THEN @columnName+N''.ToString()'' WHEN @columnType COLLATE DATABASE_DEFAULT IN (N''datetime'',N''datetime2'',N''datetimeoffset'',N''time'') THEN N''CONVERT(nvarchar(50),''+@columnName+N'',126)'' ELSE @columnName END)
+		
+		IF (@columnType COLLATE DATABASE_DEFAULT IN (N''geography'',N''geometry''))
+		BEGIN
+			SET @whereCondition = N''('' + @whereColumnName+N''.ToString() LIKE N''''''+@innerValuePattern+N'''''' COLLATE DATABASE_DEFAULT''
+			
+			IF (@compatibilityLevel>=130 AND ((@columnType COLLATE DATABASE_DEFAULT = N''geometry'' AND @geometry IS NOT NULL)
+				OR (@columnType COLLATE DATABASE_DEFAULT = N''geography'' AND @geography IS NOT NULL)))
+				SET @whereCondition = @whereCondition + N'' OR COALESCE(''+@whereColumnName+N''.STContains(''+IIF(@columnType COLLATE DATABASE_DEFAULT = N''geometry'',N''geometry::Parse(''''''+@geometry.ToString()+N'''''')),0)=1)'',N''geography::Parse(''''''+@geography.ToString()+N'''''')),0)=1)'')
+			ELSE
+				SET @whereCondition = @whereCondition + N'')''
+
+			SET @whereColumnName = @whereColumnName+N''.ToString()''
+		END
+		ELSE
+			SET @whereCondition = N''('' + @whereColumnName+N'' LIKE ''''''+@innerValuePattern+N'''''')''
 
 		SET @whereClause = @whereClause + @whereCondition + N'' OR ''
 
-		SET @columnListSelect= @columnListSelect + N''IIF(SUM(CASE WHEN ''+@whereCondition+'' THEN 1 ELSE 0 END)>0,''''''+@columnName+'','''',NULL),''
+		SET @columnListSelect = @columnListSelect + N''IIF(SUM(CASE WHEN ''+@whereCondition+'' THEN 1 ELSE 0 END)>0,''''''+@whereColumnName+'' AS ''+@columnName+'','''',NULL),''
+		SET @columnList = @columnList + N''IIF(SUM(CASE WHEN ''+@whereCondition+'' THEN 1 ELSE 0 END)>0,''''''+@columnName+'','''',NULL),''
 	END
 	ELSE
 	BEGIN
 		SET @whereClause = NULL
 		SET @columnListSelect= @columnListSelect + N''MAX('''''' + @columnName + N'',''''),''
+		SET @columnList = @columnList + N''MAX('''''' + @columnName + N'',''''),''
 	END
 
 	SET @oldDbName = @dbName
@@ -169,7 +211,7 @@ BEGIN
 	IF (@outerDbName NOT IN ('master','msdb','tempdb', 'model', 'ReportServer') AND (@dbSearchPattern IS NULL OR @outerDbName LIKE @dbSearchPattern))
 	BEGIN
 		DECLARE @statementForDatabase NVARCHAR(MAX) = REPLACE(@statement,N'[?]','['+@outerDbName+']')
-		EXECUTE sp_executesql @statementForDatabase,N'@innerValuePattern nvarchar(1000), @innerColumnSearchPattern nvarchar(256)',@innerValuePattern = @valuePattern, @innerColumnSearchPattern = @columnSearchPattern
+		EXECUTE sp_executesql @statementForDatabase,N'@innerValuePattern nvarchar(1000), @innerColumnSearchPattern nvarchar(256), @innerSchemaSearchPattern nvarchar(256), @innerColumnTypeSearchPattern nvarchar(256)',@innerValuePattern = @valuePattern, @innerColumnSearchPattern = @columnSearchPattern, @innerSchemaSearchPattern = @schemaSearchPattern, @innerColumnTypeSearchPattern = @columnTypeSearchPattern
 	END
 	FETCH NEXT FROM [databases] INTO @outerDbName
 END
@@ -178,8 +220,8 @@ CLOSE [databases];
 DEALLOCATE [databases];
 
 UPDATE #Output
-SET [MatchingSelect]=REPLACE([MatchingSelect],'[??]',[MatchingColumns]+',*')
+SET [MatchingSelect]=REPLACE([MatchingSelect],'[??]',[MatchingWhereColumns]+',*')
 
-SELECT * FROM #Output
+SELECT [Database],[Schema],[Table],[FullTableName],[MatchingColumns],[MatchingSelect] FROM #Output
 
 END
